ec141d5e706eab029d05cc98338a082a96454b3a
Andi Heinrich <andi@wafna.de>
2010-07-06 18:12:04 +0200

    Neuer Hau-den-Lukas Code (Zeit vom µC lesen)
    Zeit wird jetzt als 1 Byte vom seriellen Anschluss gelesen anstatt Steuerleitungen pollen und Zeit messen.
    Überflüssigen Heartbeat-Code entfernt, Heartbeat wird nur im Interactive-Mode benötigt.

--- Apps/Lukas.py
+++ Apps/Lukas.py
@@ -1,29 +1,16 @@
-import socket, time, threading, serial
+import socket, time, serial
 
-CCC_HOST = '10.150.89.11'
+CCC_HOST = "localhost"
+#CCC_HOST = '10.150.89.40'
 #CCC_HOST = '10.150.86.39'
-#CCC_HOST = '192.168.42.1'
-#CCC_HOST = "localhost"
 CCC_PORT = 5000
 
 net = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 net.connect((CCC_HOST, CCC_PORT))
 
-myport = serial.Serial("/dev/ttyUSB0")
-
-myport.setRTS(0)
-myport.setDTR(1)
-
-class Heartbeat(threading.Thread):
-  def __init__(self, ccc_socket):
-    threading.Thread.__init__(self)
-    self.setDaemon(True)
-    self.ccc_socket = ccc_socket
-  def run(self):
-    while True:
-      try: self.ccc_socket.send("HB")
-      except socket.error: pass
-      time.sleep(1)
+myport = serial.Serial("/dev/ttyACM0")
+#myport = serial.Serial("/dev/ttyUSB0")
+myport.baudrate = 9600
 
 def send(what):
   try:
@@ -31,23 +18,32 @@ def send(what):
   except socket.error:
     pass
 
-#hb = Heartbeat(net)
-#hb.start()
-
 lukas_time = 1.0
 
 while True:
   send("LU0")
   print "reset"
-  while myport.getDSR(): pass
-  starttime = time.time()
-  while not myport.getCTS(): 
-    lukas_time = time.time() - starttime
-    if lukas_time > 0.5: break
-  if lukas_time < 0.5:
-    lukas = 0.005 / lukas_time
-    send("LU" + str(lukas))
-    print str(int(lukas*100)) + "%"
-  while not myport.getDSR(): pass
+  lukas_time = float(ord(myport.read()))
+  print "Zeit: %d" % (lukas_time)
+  lukas =  50 / lukas_time
+  send("LU" + str(lukas))
+  print str(lukas) + "%"
+  time.sleep(2)
 
+
+# old method (polling serial control lines):
+
+#myport.setRTS(0)
+#myport.setDTR(1)
+
+#  while myport.getDSR(): pass
+#  starttime = time.time()
+#  while not myport.getCTS(): 
+#    lukas_time = time.time() - starttime
+#    if lukas_time > 0.5: break
+#  if lukas_time < 0.5:
+#    lukas = 0.005 / lukas_time
+#    send("LU" + str(lukas))
+#    print str(int(lukas*100)) + "%"
+#  while not myport.getDSR(): pass
 
