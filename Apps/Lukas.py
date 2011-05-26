09409b2cd38d53ac1d8e147e685802bb069d7338
Andi Heinrich <andi@wafna.de>
2011-05-26 22:41:13 +0200

    Hau den Lukas Code aufgeräumt

--- Apps/Lukas.py
+++ Apps/Lukas.py
@@ -1,16 +1,14 @@
 import socket, time, serial
 
 CCC_HOST = "localhost"
-#CCC_HOST = '10.150.89.40'
-#CCC_HOST = '10.150.86.39'
 CCC_PORT = 5000
+SERIAL_PORT = "/dev/ttyACM0"
+SERIAL_BAUD = 9600
 
 net = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 net.connect((CCC_HOST, CCC_PORT))
 
-myport = serial.Serial("/dev/ttyACM0")
-#myport = serial.Serial("/dev/ttyUSB0")
-myport.baudrate = 9600
+myport = serial.Serial(port=SERIAL_PORT, baudrate=SERIAL_BAUD)
 
 def send(what):
   try:
@@ -18,32 +16,19 @@ def send(what):
   except socket.error:
     pass
 
-lukas_time = 1.0
+min_lukas = 0.01
+min_t = 50.0
+
+A = (1.0-min_lukas) / ((1.0/min_t) - (1.0/255.0))
+B = (1.0-min_lukas) / (1.0 - (min_t/255.0)) - 1.0
 
 while True:
   send("LU0")
   print "reset"
-  lukas_time = float(ord(myport.read()))
+  lukas_time = ord(myport.read())
   print "Zeit: %d" % (lukas_time)
-  lukas =  50 / lukas_time
+  lukas =  A / lukas_time - B
   send("LU" + str(lukas))
-  print str(lukas) + "%"
+  print str(int(lukas*100)) + "%"
   time.sleep(2)
-
-
-# old method (polling serial control lines):
-
-#myport.setRTS(0)
-#myport.setDTR(1)
-
-#  while myport.getDSR(): pass
-#  starttime = time.time()
-#  while not myport.getCTS(): 
-#    lukas_time = time.time() - starttime
-#    if lukas_time > 0.5: break
-#  if lukas_time < 0.5:
-#    lukas = 0.005 / lukas_time
-#    send("LU" + str(lukas))
-#    print str(int(lukas*100)) + "%"
-#  while not myport.getDSR(): pass
 
