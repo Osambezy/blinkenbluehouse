e2b81c7b508c8ffbf10be8a98889fb4d3b0c9c26
Andi Heinrich <andi@wafna.de>
2011-05-27 04:13:46 +0200

    VU-Meter rewrite ohne Threads Alten, fehleranfälligen Threading-Code rausgehauen. Spart deutlich Latenzzeit.

--- Apps/VU-Meter.py
+++ Apps/VU-Meter.py
@@ -17,100 +17,6 @@ CCC_PORT = 5000
 TARGET_HEIGHT =  7
 TARGET_CHANNELS = 2
 
-net = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-net.connect((CCC_HOST, CCC_PORT))
-
-class AudioGrabber(threading.Thread):
-  def __init__(self, mode, buffersize, framesize=1024, rate=22050, format="S16_LE"):
-    if format not in ["U8", "S16_LE"]:
-      raise Exception, "Error: unsupported audio parameters"
-    threading.Thread.__init__(self)
-    self.setDaemon(True)
-    self.mode = mode
-    self.queue = Queue.Queue(buffersize)
-    if mode=="alsa":
-      self.framesize = framesize
-      self.rate = rate
-      self.format = format
-      if self.format == "U8":
-        self.fmt = (self.framesize * 1, self.framesize * "B", 128, 128)    # (number of bytes per frame, struct fmt, offset, max amplitude)
-      elif self.format == "S16_LE":
-        self.fmt = (self.framesize * 2, "<" + self.framesize * "h", 0, 32768)
-    self.quit = False
-
-  def run(self):
-    print "Recording audio..."
-    if self.mode=="alsa":
-      command = "arecord -N -c 1 -r %d -f %s" % (self.rate, self.format)
-      pipe = os.popen(command, "r", 0)
-      while True:
-        if self.quit:
-          pipe.close()
-          exit()
-        frame = pipe.read(self.fmt[0])
-        if len(frame) == self.fmt[0]:
-          rms = 0.0
-          for sample in struct.unpack(self.fmt[1], frame):
-            rms += (float(sample - self.fmt[2]) / self.fmt[3]) ** 2
-          rms = math.sqrt(rms/self.framesize)
-          self.push(rms)
-        else:
-          print "Error in audio subprocess, restarting..."
-          pipe.close()
-          pipe = os.popen(command, "r", 0)
-
-    elif self.mode=="jack": 
-      jack.attach("VU-Meter")
-      jack.register_port("in_1", jack.IsInput)
-      jack.register_port("out_1", jack.IsOutput)
-      jack.activate()
-      #jack.connect("alsa_pcm:capture_1", "VU-Meter:in_1")
-      jack.connect("system:capture_1", "VU-Meter:in_1")
-      self.framesize = jack.get_buffer_size()
-      self.rate = jack.get_sample_rate()
-      input_array = numpy.zeros((1, self.framesize), 'f')
-      output_array = numpy.zeros((1, self.framesize), 'f')
-      time_sync = time.time()
-      sync_interval = 1.0 / self.rate * self.framesize
-      while True:
-        time_sync += sync_interval
-        try:
-          time.sleep(time_sync - time.time()) # workaround because jack.process() blocks without giving control to other threads
-        except IOError:
-          pass
-        if self.quit:
-          jack.deactivate()
-          jack.detach()
-          exit()
-        try:          
-          jack.process(output_array, input_array)
-        except (jack.InputSyncError, jack.OutputSyncError):
-          print "Jack out of sync"
-        rms = math.sqrt(numpy.sum(numpy.power(input_array[0],2))/self.framesize)
-        self.push(rms)
-
-  def push(self, value):
-    try:
-      self.queue.put(value, block=False)
-    except Queue.Full:
-      print "Buffer overflow! Reduce audio rate or increase buffer size."
-      self.queue.join()
-      if self.mode=="alsa": pipe.flush()
-
-def scale_min_max(window):
-  mean = 0
-  for item in window: mean += item
-  mean = float(mean) / len(window)
-  stdev = 0
-  for item in window: stdev += (item - mean)**2
-  stdev = math.sqrt(float(stdev) / len(window))
-  return (mean - 1.5 * stdev, mean + 1.5 * stdev)
-
-a = AudioGrabber(mode=AUDIO_MODE, buffersize=2)
-a.start()
-
-time_update = time.time()
-time_random = time_update
 UPDATE_INTERVAL = 0.1
 RANDOMIZER_INTERVAL = 1.0
 DECAY = 10     # decay in pixels/sec
@@ -125,21 +31,53 @@ scale_window = [0]
 scale_windowsize = int(300 / UPDATE_INTERVAL)
 vu_value = TARGET_CHANNELS * [0]
 random_gain = TARGET_CHANNELS * [1.0]
-net.send("VU")
 
-while True:
-    try:
-      rms = a.queue.get()
-    except KeyboardInterrupt:
-      a.quit = True
-      a.join()
-      exit()
+net = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+net.connect((CCC_HOST, CCC_PORT))
+net.send("VU")
+time_update = time.time()
+time_random = time_update
+
+if AUDIO_MODE=="alsa":
+      framesize = 1024
+      rate = 22050
+      pipe = os.popen("arecord -N -c 1 -r %d -f S16_LE" % (rate,), "r", 0)
+elif AUDIO_MODE=="jack": 
+      jack.attach("VU-Meter")
+      jack.register_port("in_1", jack.IsInput)
+      jack.register_port("out_1", jack.IsOutput)
+      jack.activate()
+      #jack.connect("alsa_pcm:capture_1", "VU-Meter:in_1")
+      jack.connect("system:capture_1", "VU-Meter:in_1")
+      framesize = jack.get_buffer_size()
+      rate = jack.get_sample_rate()
+      input_array = numpy.zeros((1, framesize), 'f')
+      output_array = numpy.zeros((1, framesize), 'f')
+
+try:
+  while True:
+    if AUDIO_MODE=="alsa":
+        rms = 0.0
+        frame = pipe.read(framesize * 2) # 2 bytes per sample (16-bit)
+        if len(frame) == framesize * 2:
+          for sample in struct.unpack("<" + framesize * "h", frame):
+            rms += (sample / 32768.0) ** 2
+          rms = math.sqrt(rms/framesize)
+        else:
+          print "Error in audio subprocess, restarting..."
+          pipe.close()
+          pipe = os.popen(command, "r", 0)
+    elif AUDIO_MODE=="jack":
+        try:          
+          jack.process(output_array, input_array)
+        except (jack.InputSyncError, jack.OutputSyncError):
+          print "Jack out of sync"
+        rms = math.sqrt(numpy.sum(numpy.power(input_array[0],2))/framesize)
     if rms > 0:
       volume = math.log(rms, 1.15)
     else:
       volume = SILENCE
     if volume > maxvolume: maxvolume = volume
-    a.queue.task_done()
     if time.time() >= time_update:
       scaled = (maxvolume-scale_min)/(scale_max-scale_min) * TARGET_HEIGHT
       scaled = min(scaled, 1.3*TARGET_HEIGHT, 254)
@@ -154,7 +92,13 @@ while True:
       try: net.send(command)
       except socket.error: pass
       scale_window = scale_window[-scale_windowsize:] + [maxvolume]
-      scale_min, scale_max = scale_min_max(scale_window)
+      mean = 0.0
+      for item in scale_window: mean += item
+      mean = mean / len(scale_window)
+      stdev = 0.0
+      for item in scale_window: stdev += (item - mean)**2
+      stdev = math.sqrt(stdev / len(scale_window))
+      scale_min, scale_max = mean - 1.5 * stdev, mean + 1.5 * stdev
       print "Scaling: ", int(scale_min*10)/10.0, int(scale_max*10)/10.0
       while time.time()>=time_update: time_update += UPDATE_INTERVAL
       maxvolume = SILENCE
@@ -162,4 +106,12 @@ while True:
       random_gain[0] = MAX_RANDOM_GAIN ** random.uniform(-1.0,1.0)
       random_gain[1] = 1 / random_gain[0]
       while time.time()>=time_random: time_random += RANDOMIZER_INTERVAL
+except KeyboardInterrupt: pass
+
+if AUDIO_MODE=="alsa":
+  pipe.close()
+elif AUDIO_MODE=="jack":
+  jack.deactivate()
+  jack.detach()
+
 
